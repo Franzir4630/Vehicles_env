b1b92c5515f9dd77427e601b782ad3fc59b289cc
Franzir4630 <cesarzirate@gmail.com>
2025-03-22 19:21:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as xp
+import plotly.express as px
 
 # Leemos el archivo csv
 df_vehicles = pd.read_csv('vehicles_us.csv')
@@ -12,22 +12,22 @@ st.header('Analisis de vehiculos')
 hist_button = st.button('Construir histograma') # crear un botón para el histograma
 disp_button = st.button('Construir gráfico de dispersión') # Crear botón para el gráfico de dispersión
 
-        if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
-            # crear un histograma
-            fig = px.histogram(df_vehicles, x="odometer")
+    # crear un histograma
+    fig = px.histogram(df_vehicles, x="odometer")
         
-            # mostrar un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
             
-        if disp_button: # al hacer click en el botón
-            # escribir el mensaje
-            st.write('creación de un gráfico de dispersión para el conjunto de datos de anuncios de ventas de coches')
+if disp_button: # al hacer click en el botón
+    # escribir el mensaje
+    st.write('creación de un gráfico de dispersión para el conjunto de datos de anuncios de ventas de coches')
 
-            # Creamos el gráfico de dispersión
-            fig = px.scatter(df_vehicles, x='odometer', y='price')
+    # Creamos el gráfico de dispersión
+    fig = px.scatter(df_vehicles, x='odometer', y='price')
 
-            # Mostramos un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
+    # Mostramos un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
